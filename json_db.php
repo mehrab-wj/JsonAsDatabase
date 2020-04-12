a737fde4079fbaf7d4a815c31e0ba4d56822e062
iMehrab <white_joker20@yahoo.com>
2020-04-12 19:47:06 +0430

    Add files via upload

---
+++ json_db.php
@@ -0,0 +1,31 @@
+<?php
+class JsonDatabase {
+    private $path;
+    private $autosave = true;
+    public $db;
+    public function __construct($path,$autosave = true) {
+      $this->path = $path;
+      $this->autosave = $autosave;
+      $this->load();
+    }
+    public function load() {
+      if (file_exists($this->path)) {
+        $this->db =  json_decode(file_get_contents($this->path));
+      }
+      else { $this->db = json_decode([]); }
+    }
+    public function save() {
+      if (file_exists($this->path)) {
+        return file_put_contents($this->path, json_encode($this->db,JSON_PRETTY_PRINT));
+      }
+      return false;
+    }
+    public function __destruct() {
+        if ($this->autosave) {
+            $this->save();
+        }
+    }
+}
+
+
+?>
